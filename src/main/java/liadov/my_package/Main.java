1044a6e290561bf65d94ed749114bb9e5a923e49
Aleksandr Liadov <aleksandr_liadov@epam.com>
2021-03-23 23:05:25 +0400

    update println to hello world

--- src/main/java/liadov/my_package/Main.java
+++ src/main/java/liadov/my_package/Main.java
@@ -2,6 +2,6 @@ package liadov.my_package;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("Hello World");
     }
 }
