27d5225e0d6029d8d153e4f1bed2ab64924e4b33
Aleksandr Liadov <aleksandr_liadov@epam.com>
2021-03-26 23:58:19 +0400

    CacheElement class: add methods: toString, hashCode, equals

--- src/main/java/liadov/mypackage/lesson2/CacheElement.java
+++ src/main/java/liadov/mypackage/lesson2/CacheElement.java
@@ -1,5 +1,7 @@
 package liadov.mypackage.lesson2;
 
+import java.util.Objects;
+
 public class CacheElement <T>{
     T element;
     int index;
@@ -7,5 +9,26 @@ public class CacheElement <T>{
     public CacheElement(T element, int index){
         this.element = element;
         this.index = index;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CacheElement)) return false;
+        CacheElement<?> that = (CacheElement<?>) o;
+        return index == that.index && element.equals(that.element);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(element, index);
+    }
+
+    @Override
+    public String toString() {
+        return "CE{" +
+                "e=" + element +
+                ", i=" + index +
+                '}';
     }
 }
