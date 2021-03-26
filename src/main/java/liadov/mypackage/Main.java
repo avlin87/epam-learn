8752b30f14a598217b7729dd46f02e6d748b2c00
Aleksandr Liadov <aleksandr_liadov@epam.com>
2021-03-27 00:02:32 +0400

    update Main class with examples

--- src/main/java/liadov/mypackage/Main.java
+++ src/main/java/liadov/mypackage/Main.java
@@ -11,18 +11,18 @@ public class Main {
 
         Cache<String> cache = new Cache<>(3);
 
-        cache.add("one");
+        cache.add("one",1);
         System.out.println("add(\"one\"), result: " + cache);
-        cache.add("two");
+        cache.add("two",2);
         System.out.println("add(\"two\"), result: " + cache);
-        cache.add("three");
+        cache.add("three",3);
         System.out.println("add(\"three\"), result: " + cache);
-        cache.add("four");
+        cache.add("four",4);
         System.out.println("add(\"four\"), result: " + cache);
         cache.delete("two");
         System.out.println("delete(\"two\"), result: " + cache);
         System.out.println("isPresent(\"four\"): " + cache.isPresent("four"));
-        System.out.println("get(\"three\"): \"" + cache.get("three") + "\" " + cache);
+        System.out.println("get(\"3\"): \"" + cache.get(3) + "\" " + cache);
         cache.clear();
         System.out.println("cache.clear(), result: " + cache);
 
@@ -39,6 +39,11 @@ public class Main {
         System.out.println("delete(): " + storage);
         System.out.println("storage.getLast(): " + storage.getLast());
         System.out.println("storage.get(1): " + storage.get(1) + " - " + storage);
+        System.out.println("storage.get(2): " + storage.get(2) + " - " + storage);
+        System.out.println("storage.get(0): " + storage.get(0) + " - " + storage);
+        System.out.println("storage.get(1): " + storage.get(1) + " - " + storage);
+        storage.delete();
+        System.out.println("delete(): " + storage);
         storage.clear();
         System.out.println("storage.clear: " + storage);
     }
