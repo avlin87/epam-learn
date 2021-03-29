1329c4da880bfe9ad8dcb02e9af80507873679f4
Aleksandr Liadov <aleksandr_liadov@epam.com>
2021-03-30 01:26:44 +0400

    fix JavaDoc in User class

--- src/main/java/liadov/mypackage/lesson3/User.java
+++ src/main/java/liadov/mypackage/lesson3/User.java
@@ -9,6 +9,10 @@ public class User {
         this.role = role;
     }
 
+    /**
+     * 8. Method receives object of User class and prints hello message based on user full name and user role
+     * @param user this object is used to print hello message
+     */
     public static void sayHello(User user) {
         System.out.printf("Приветствуем %s с ролью %s", user.fullName, user.role);
     }
