be0a4ba237cab855d956ebb3cd77d16c488be3aa
Aleksandr Liadov <aleksandr_liadov@epam.com>
2021-03-29 23:24:12 +0400

    create class Human

---
+++ src/main/java/liadov/mypackage/lesson3/Human.java
@@ -0,0 +1,13 @@
+package liadov.mypackage.lesson3;
+
+public class Human {
+    private String fullName;
+    private int age;
+    private Address address;
+
+    public Human(String fullName, int age, Address address) {
+        this.fullName = fullName;
+        this.age = age;
+        this.address = address;
+    }
+}
