4382cf2480acb0728aebce57166845c88b0b81f4
Aleksandr Liadov <aleksandr_liadov@epam.com>
2021-04-04 21:48:20 +0400

    remove slf4j-api dependency from POM

--- src/main/java/liadov/mypackage/lesson4/ElementDoesNotExistException.java
+++ src/main/java/liadov/mypackage/lesson4/ElementDoesNotExistException.java
@@ -1,5 +1,9 @@
 package liadov.mypackage.lesson4;
 
+/**
+ * Thrown to indicate that an array has been accessed with an illegal index.
+ * The index is either negative or greater than or equal to the size of the array.
+ */
 public class MyUncheckedArrayIndexOutOfBoundsException extends ArrayIndexOutOfBoundsException {
     public MyUncheckedArrayIndexOutOfBoundsException() {
         super();
