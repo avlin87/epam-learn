f12a9d47be109a8f1f7868afdd26cad86a34c129
Aleksandr Liadov <aleksandr_liadov@epam.com>
2021-04-04 22:37:43 +0400

    rename MyUncheckedArrayIndexOutOfBoundsException class to ElementDoesNotExistException, add javaDoc

--- src/main/java/liadov/mypackage/lesson4/ElementDoesNotExistException.java
+++ src/main/java/liadov/mypackage/lesson4/ElementDoesNotExistException.java
@@ -4,24 +4,43 @@ package liadov.mypackage.lesson4;
  * Thrown to indicate that an array has been accessed with an illegal index.
  * The index is either negative or greater than or equal to the size of the array.
  */
-public class MyUncheckedArrayIndexOutOfBoundsException extends ArrayIndexOutOfBoundsException {
-    public MyUncheckedArrayIndexOutOfBoundsException() {
+public class ElementDoesNotExistException extends ArrayIndexOutOfBoundsException {
+
+    /**
+     * Constructs an ElementDoesNotExistException with no detail message.
+     */
+    public ElementDoesNotExistException() {
         super();
     }
 
-    public MyUncheckedArrayIndexOutOfBoundsException(int index) {
+    /**
+     * Constructs a new ElementDoesNotExistException class with an argument indicating the illegal index.
+     *
+     * @param index - the illegal index
+     */
+    public ElementDoesNotExistException(int index) {
         super(index);
     }
 
-    public MyUncheckedArrayIndexOutOfBoundsException(String s) {
+    /**
+     * Constructs an ElementDoesNotExistException class with the specified detail message.
+     *
+     * @param s - the detail message
+     */
+    public ElementDoesNotExistException(String s) {
         super(s);
     }
 
-    public String getFullStackTrace(){
+    /**
+     * Method return String message formed from StackTraceElements of Exception
+     *
+     * @return String
+     */
+    public String getFullStackTrace() {
         StringBuilder sb = new StringBuilder();
         sb.append(this.toString());
-        for (StackTraceElement element:super.getStackTrace()) {
-            sb.append("\n\t"+element.toString());
+        for (StackTraceElement element : super.getStackTrace()) {
+            sb.append("\n\t" + element.toString());
         }
         return sb.toString();
     }
