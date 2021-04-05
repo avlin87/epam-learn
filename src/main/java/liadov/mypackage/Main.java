b4f5e561e298a21dfb7cf90d700b6dfd45daeb49
Aleksandr Liadov <aleksandr_liadov@epam.com>
2021-04-05 22:32:05 +0400

    add read from console to Main

--- src/main/java/liadov/mypackage/Main.java
+++ src/main/java/liadov/mypackage/Main.java
@@ -1,9 +1,32 @@
 package liadov.mypackage;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Scanner;
+
+@Slf4j
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
 
+        
+
+        System.out.println("\nWelcome to lesson5 HomeWork!\nPlease type \"help\"");
+        readConsoleCommand();
+
+    }
+
+    private static void readConsoleCommand() {
+        String commandTemp = "";
+        boolean continueRun = true;
+
+        Scanner scanner = new Scanner(System.in);
+        while (continueRun&&scanner.hasNext()) {
+            if (scanner.hasNext()) {
+                commandTemp= scanner.next();
+                log.info("Command read: {}",commandTemp);
+            }
+        }
     }
 }
