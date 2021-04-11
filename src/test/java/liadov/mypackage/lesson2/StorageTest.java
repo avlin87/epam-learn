e79f8f1dac2d33985c83bb2abc4fb8a029e36979
Aleksandr Liadov <aleksandr_liadov@epam.com>
2021-04-12 01:45:48 +0400

    Revert "add CacheElementTest"
    This reverts commit 5901570cf22e8ddd053770fde16e96a1bcac4205.

--- src/test/java/liadov/mypackage/lesson2/StorageTest.java
+++ src/test/java/liadov/mypackage/lesson2/StorageTest.java
@@ -1,7 +1,6 @@
 package liadov.mypackage.lesson2;
 
 import org.junit.jupiter.api.Test;
-import liadov.mypackage.lesson4.ElementDoesNotExistException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,41 +10,23 @@ class StorageTest {
     void addShouldPutElementToStorage() {
         Storage<String> storage = new Storage<>();
         storage.add("testElement");
-        assertEquals(storage.getLast(), "testElement");
+        assertTrue("testElement".equals(storage.getLast()));
     }
 
     @Test
-    void deleteShouldRemoveLastElement() {
-        Storage<String> storage = new Storage<>(new String[]{"testElement 3"});
-        assertNotNull(storage.get(0));
-        storage.delete();
-        assertNull(storage.get(0));
+    void delete() {
     }
 
     @Test
-    void clearShouldRemoveAllFromStorage() {
-        Storage<String> storage = new Storage<>(new String[]{"testElement 1","testElement 2","testElement 3"});
-        assertNotNull(storage.get(0));
-        storage.clear();
-        assertNull(storage.get(0));
+    void clear() {
     }
 
     @Test
-    void getLastShouldReturnLastElement() {
-        Storage<String> storage = new Storage<>(new String[]{"testElement 1","testElement 2","testElement 3"});
-        assertEquals(storage.getLast(), "testElement 3");
+    void getLast() {
     }
 
     @Test
-    void getElementByIndex() {
-        Storage<String> storage = new Storage<>(new String[]{"testElement 1","testElement 2","testElement 3"});
-        assertEquals(storage.get(1), "testElement 2");
-    }
-
-    @Test
-    void getReturnNull() {
-        Storage<String> storage = new Storage<>(new String[]{"testElement 1","testElement 2","testElement 3"});
-        assertNull(storage.get(50));
+    void get() {
     }
 
     @Test
