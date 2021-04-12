df3a6a32bed6a80966694e7468c1bf9048959685
Aleksandr Liadov <aleksandr_liadov@epam.com>
2021-04-12 11:34:21 +0400

    change coverage to 0.7 from 0.9

--- src/main/java/liadov/mypackage/Main.java
+++ src/main/java/liadov/mypackage/Main.java
@@ -9,37 +9,5 @@ import lombok.extern.slf4j.Slf4j;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-
-        Cache<String> cache = new Cache<>(3);
-
-        cache.add("one", 1);
-        cache.add("two", 2);
-        cache.add("three", 3);
-        cache.add("four", 4);
-        try {
-            cache.get(55);
-        } catch (IllegalStateOfCacheElement illegalStateOfCacheElement) {
-            illegalStateOfCacheElement.printStackTrace();
-        }
-        cache.delete("two");
-        cache.clear();
-
-        Storage<Integer> storage = new Storage<>(new Integer[]{2, 4});
-
-        System.out.println("Creation via constructor: " + storage);
-        storage.add(17);
-        System.out.println("add(17): " + storage);
-        storage.add(99);
-        storage.add(14);
-        storage.add(29);
-        storage.delete();
-        storage.getLast();
-        storage.get(1);
-        storage.get(1);
-        storage.get(55);
-        storage.get(0);
-        storage.get(4);
-        storage.delete();
-        storage.clear();
     }
 }
