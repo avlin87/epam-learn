413ef39f3a85332bb6ef549c965052f8b68e3fed
Aleksandr Liadov <aleksandr_liadov@epam.com>
2021-04-13 18:27:03 +0400

    get OutOfMemoryError

--- src/main/java/liadov/mypackage/Main.java
+++ src/main/java/liadov/mypackage/Main.java
@@ -1,7 +1,26 @@
 package liadov.mypackage;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
+    static final List<String[]> arrays = new LinkedList<>();
+
+
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        long start = System.currentTimeMillis();
+        try {
+            for (int i = 0; i < 10000; i++) {
+                String[] strings = new String[10000];
+                for (int j = 0; j < strings.length - 1; j++) {
+                    strings[j]=String.valueOf(System.currentTimeMillis());
+                }
+                arrays.add(strings);
+            }
+        } finally {
+            System.out.println((System.currentTimeMillis() - start) / 60000.0);
+        }
     }
 }
