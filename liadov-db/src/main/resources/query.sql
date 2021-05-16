7c922c449caedd3e031d342010665f771ab8946e
Aleksandr Liadov <aleksandr_liadov@epam.com>
2021-05-16 16:06:39 +0400

    remove new line before semicolon

--- liadov-db/src/main/resources/query.sql
+++ liadov-db/src/main/resources/query.sql
@@ -1,44 +1,36 @@
 select *
 from products p
-where upper(p.title) like upper('academy q%')
-;
+where upper(p.title) like upper('academy q%');
 
 select *
 from products p
 where p.price = 9.99
   and p.category = 8
-order by p.category, p.price
-;
+order by p.category, p.price;
 
 select *
 from products p
-where p.category in (8, 15)
-;
+where p.category in (8, 15);
 
 select *
 from products p
-where p.price between 10 and 20
-;
+where p.price between 10 and 20;
 
 select *
 from orders o
-where o.orderdate between date ('2004-01-05') and date ('2004-02-05')
-;
+where o.orderdate between date ('2004-01-05') and date ('2004-02-05');
 
 select o.customerid, count(*)
 from orders o
-group by o.customerid
-;
+group by o.customerid;
 
 select o.customerid, o.orderdate, sum(o.totalamount) as sumtotalamount
 from orders o
 group by o.customerid, o.orderdate
-having sum(o.totalamount) > 100
-;
+having sum(o.totalamount) > 100;
 
 select cust.firstname, cust.lastname, prod.title, o.orderdate
 from customers cust
          join orders o on cust.customerid = o.customerid
          join orderlines ordLine on o.orderid = ordline.orderid
-         join products prod on ordLine.prod_id = prod.prod_id
-;
+         join products prod on ordLine.prod_id = prod.prod_id;
